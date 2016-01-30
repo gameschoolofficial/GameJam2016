17afb48f48cc6d3d0b3cd20ee80082fa211887bf
Charlie Trinh <charlie@game-school.com>
2016-01-30 15:51:52 -0800

    added a new scene called GameRoom that will be used as staging the different levels.  Use other scenes before bringing into this main scene.

--- Assets/_scripts/spawnTarget.cs
+++ Assets/_scripts/spawnTarget.cs
@@ -8,19 +8,12 @@ public class spawnTarget : MonoBehaviour {
 	public float spawnDuration = 4f;
 	public GameObject targetBoardObjects;
 	public Transform targetSpawnPoint;
-
-
-
-	// when OFF, only spawning target is available
-	// when ON, activate timer before activating off state
-	//private enum targetState{ STATE_OFF, STATE_ON, STATE_TRANSITION };
-	//targetState myState = targetState.STATE_OFF;
-
+	
 	private GameObject targetInstance;
 
 	// Use this for initialization
 	void Start () {
-		//resetTargetObject ();
+
 	}
 	
 	// Update is called once per frame
@@ -30,10 +23,8 @@ public class spawnTarget : MonoBehaviour {
 
 	public void targetOn(){
 
-		print ("activating Target ON");
 		//instantiate a prefab target
 		targetInstance = Instantiate (targetBoardObjects,targetSpawnPoint.position, targetSpawnPoint.rotation) as GameObject;
-		print ("Position="+targetSpawnPoint.position);
 		//choose the type of points to give the prefab
 
 	}
@@ -41,10 +32,6 @@ public class spawnTarget : MonoBehaviour {
 	public void targetOff(){
 		Destroy (targetInstance);
 	}
-
-	void resetTargetObject(){
-
-		//destroy all instances created?
-	}
+	
 
 }
