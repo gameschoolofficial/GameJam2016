ccf51bc9bd19a6506bb60ca8d9d84401e48ccf5e
Charlie Trinh <charlie@game-school.com>
2016-02-16 18:20:42 -0800

    Can't figure out what's going on with the score function.

--- Assets/_scripts/objectDestroy.cs
+++ Assets/_scripts/objectDestroy.cs
@@ -5,27 +5,32 @@ public class objectDestroy : MonoBehaviour {
 
 	//public GameObject spawnMgr;
 	//private spawningManager mgrInstance;
+	//public scoreManager scoreMgr; 
+	public GameObject scoreObj;
+	private scoreManager scoreInst;
 
 	public void destroyObject(){
 	
 		//print ("destroy - gameObject type = "+gameObject.name);
-		/*
-		mgrInstance = spawnMgr.GetComponent<spawningManager> () as spawningManager;
-		print (mgrInstance);
 
+		//mgrInstance = spawnMgr.GetComponent<spawningManager> () as spawningManager;
+		//print (mgrInstance);
+		scoreInst = scoreObj.GetComponent<scoreManager> () as scoreManager;
 		if (gameObject.name == "targetBoardSmall(Clone)") {
 		
-			mgrInstance.countTargetHit();
+			scoreInst.addToScore(1);
+			//mgrInstance.countTargetHit();
 			//spawnMgr.countTargetHit();
 			print ("destroying smalltarget");
 		};
 		if (gameObject.name == "kitty(Clone)") {
 
-			mgrInstance.countKittyHit();
+			//scoreInst.addToScore(2);
+			//mgrInstance.countKittyHit();
 			//spawnMgr.countTargetHit();
 			print ("destroying kitty!");
 		};
-		*/
+
 
 		Destroy (gameObject);
 	}
---
+++ Assets/_scripts/scoreManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoreManager : MonoBehaviour {
+
+	public TextMesh gameScore;
+
+	private int numTargetKilled;
+	private int numCutiesKilled; 
+	int gameIntScore;
+
+	// Use this for initialization
+	void Awake () {
+	
+		//overallScore = 0;
+		//numTargetKilled = 0;
+		//numCutiesKilled = 0;
+		print ("initializing score to 0");
+		print ("overallScore = " + gameIntScore);
+		print ("numTargetKilled = " + numTargetKilled);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//gameScore.text = gameIntScore.ToString();
+		//print ("update: " + gameIntScore);
+	
+	}
+
+	public void addToScore(int objType){
+	
+		//1 - targets
+		//2 - cats
+
+		//Variables in here don't seem to match the local variables to this class. 
+
+		print ("addToScore score: "+gameIntScore);
+		print ("numTargetKilled: "+numTargetKilled);
+
+		if (objType == 1) {
+			numTargetKilled++;
+			print (numTargetKilled + " Targets destroyed");
+
+			//overallScore = overallScore + 100;
+			gameIntScore += 100;
+			print ("new Score "+ gameIntScore);
+		}
+		if (objType == 2) {
+
+			numCutiesKilled++;
+			//overallScore -= 1;
+			print ("add Kitty score " + gameIntScore);
+		}
+
+	}
+
+
+}
